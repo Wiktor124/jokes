0f39dd9517ae86013dd6683116bace3b3e602d9a
Víctor Peña <victortkd88@gmail.com>
2023-05-13 17:58:46 -0600

    handle errors when fetching added api

--- js/api.js
+++ js/api.js
@@ -1,20 +1,28 @@
 const API = 'https://icanhazdadjoke.com/',
-    config = Object.freeze({
-      headers: {
-        Accept: "application/json"
-      }
-    })
+  config = Object.freeze({
+    headers: {
+      Accept: "application/json"
+    }
+  })
 
 export const fetchJoke = async () => {
-  const Joke = await fetch(API, config)
-  .then(response => response.json())
+  try {
+    const Joke = await fetch(API, config)
+      .then(response => response.json())
 
-  return Joke;
+    return Joke;
+  } catch {
+    throw new Error('Faild to fetch joke!')
+  }
 }
 
 export const fetchJokes = async (kindJoke) => {
-  const jokes = await fetch(`${API}/search?term=${kindJoke}`, config)
-  .then(response => response.json())
+  try {
+    const jokes = await fetch(`${API}/search?term=${kindJoke}`, config)
+      .then(response => response.json())
 
-  return jokes;
+    return jokes;
+  } catch {
+    throw new Error('Faild to fetch jokes!')
+  }
 }
