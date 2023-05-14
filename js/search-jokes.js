56a330a98c310f0c3882c9ac92d77543d0438a86
Víctor Peña <victortkd88@gmail.com>
2023-05-14 11:43:03 -0600

    new properties

--- js/search-jokes.js
+++ js/search-jokes.js
@@ -4,14 +4,14 @@ import { fetchJokes } from "./api.js";
 const searchJokes = async () => {
   const searchThis = document.querySelector('#serach-joke__bar').value.trim();
 
-  if (!searchThis) {
+  if (searchThis === '') {
     return alert('Required Field')
   }
 
   const { results } = await fetchJokes(searchThis);
-  const searchJokesContainer = document.querySelector('.search-jokes__list');
+  const jokesContainerList = document.querySelector('.search-jokes__list');
   
-  searchJokesContainer.innerHTML = results.length === 0
+  jokesContainerList.innerHTML += results.length === 0
     ? `<li class="joke">No results</li>`
     : results.map(({ joke }) => `<li class="joke">${joke}</li>`).join('');
 }
