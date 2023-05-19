2ed98acf03f906934d5ea2d73a3cad3f009f8ec5
Víctor Peña <victortkd88@gmail.com>
2023-05-19 16:43:30 -0600

    check if the same was written

--- js/search-jokes.js
+++ js/search-jokes.js
@@ -1,12 +1,18 @@
 import { fetchJokes } from "./api.js";
 
 // search jokes by typing
+let lastSearch;
 const searchJokes = async () => {
   const searchThis = document.querySelector('#serach-joke__bar').value.trim();
-
+  
   if (searchThis === '') {
     return alert('Required Field')
   }
+  
+  if(lastSearch === searchThis) {
+    return
+  }
+  lastSearch = searchThis;
 
   const { results } = await fetchJokes(searchThis);
   const jokesContainerList = document.querySelector('.search-jokes__list');
